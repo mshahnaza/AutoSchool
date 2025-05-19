26b9af9891bd8e84e3e8567f713eb1e71aa6affd
mshahnaza <mamirovasahnaza042@gmail.com>
2025-05-19 14:16:20 +0600

    created project

---
+++ src/main/java/org/example/autoschool/AutoSchoolApplication.java
@@ -0,0 +1,13 @@
+package org.example.autoschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoSchoolApplication.class, args);
+    }
+
+}
