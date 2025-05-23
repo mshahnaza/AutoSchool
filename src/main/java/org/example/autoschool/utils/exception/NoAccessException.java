8e78a722d7139d647720987fd41555238bcd82a1
Alieva Nargiza <nargiza.alieva018@gmail.com>
2025-05-23 23:07:05 +0600

    created new No access exception

---
+++ src/main/java/org/example/autoschool/utils/exception/NoAccessException.java
@@ -0,0 +1,7 @@
+package org.example.autoschool.utils.exception;
+
+public class NoAccessException extends RuntimeException {
+    public NoAccessException(String message) {
+        super(message);
+    }
+}
