7a57c2efee6cb159d42bbabaf89c79b885ce9619
Alieva Nargiza <nargiza.alieva018@gmail.com>
2025-05-25 00:56:22 +0600

    configured email sender

---
+++ src/main/java/org/example/autoschool/config/EmailConfig.java
@@ -0,0 +1,45 @@
+package org.example.autoschool.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Slf4j
+@Configuration
+public class EmailConfig {
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        log.info("Mail Sender: {}", username);
+        log.info("Mail Password: {}", password);
+
+        System.out.printf("Mail Sender: %s\n", username);
+        System.out.printf("Mail Password: %s\n", password);
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setProtocol("smtp");
+        mailSender.setDefaultEncoding("UTF-8");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
